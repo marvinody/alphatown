8b12023c4417726f0104fa2a4e758e7bd34cbdff
Manuel Bugallo <marvinody@gmail.com>
2022-02-05 15:43:55 -0500

    --wip-- [skip ci]

---
+++ public/javascript/main.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFydmlub2R5IiwiYSI6ImNqdjJqMHQ0NDBjOGc0M2w4cjZ3NThveXIifQ.O6h9z8qT-FLrinnnqFqmBg';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11'
+});
--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use(
   })
 )
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', indexRouter);
 
