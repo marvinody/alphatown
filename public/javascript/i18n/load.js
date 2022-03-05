d0223adb85fa7e773c8c4d8e23f781b000bed6bd
Manuel Bugallo <marvinody@gmail.com>
2022-03-04 19:44:50 -0500

    adds loading of jp lang & missed translation

--- public/javascript/i18n/load.js
+++ public/javascript/i18n/load.js
@@ -1,7 +1,8 @@
 const i18nLoaded = (() => {
   return new Promise((resolve, reject) => {
     $.i18n().load({
-      'en': '/javascript/i18n/languages/en.json'
+      'en': '/javascript/i18n/languages/en.json',
+      'ja': '/javascript/i18n/languages/ja.json',
     }).done(() => {
       $('body').i18n()
       resolve()
